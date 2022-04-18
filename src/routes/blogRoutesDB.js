878154b5f3b2cf8f8ce447fa7c35b3e5c9d672b3
Diwakar Saini <diwakarsaini94@gmail.com>
2022-04-18 01:59:00 -0400

    server changes

--- src/routes/blogRoutesDB.js
+++ src/routes/blogRoutesDB.js
@@ -4,7 +4,6 @@ let user = require("../models/user");
 const { check, validationResult } = require("express-validator");
 const authMiddleware = require("../middlewares/authMiddleware");
 const authMiddlewareIsAdmin = require("../middlewares/authMiddlewareIsAdmin");
-const user = require("../models/user");
 
 const router = express.Router();
 
@@ -22,8 +21,8 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   try {
     const blogDB = await blogs.find();
-    const user = await user.findById(blogDB.author);
-    blogDB.author = user.name;
+    const userName = await user.findById(blogDB.author);
+    blogDB.author = userName.name;
     res.send(blogDB);
   } catch (err) {
     return res.status(500).send("Server error");
