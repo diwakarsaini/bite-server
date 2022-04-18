d8cefad2530b18e12170f1b2b3ef1efa730e3616
Diwakar Saini <diwakarsaini94@gmail.com>
2022-04-18 01:01:56 -0400

    server changes

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.post("/upload", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("server started");
